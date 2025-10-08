3775739d216229e4af432be0cac0c4ddeaf5cfd6
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-10-09 02:26:46 +0800

    Added expanded swagger feature and included xml comments

--- Training Management API/Program.cs
+++ Training Management API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using Training_Management_API.Data;
 using Training_Management_API.Mappings;
 using Training_Management_API.Models;
@@ -14,7 +16,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Training Management API Exercise",
+        Version = "v1",
+        Description = "API for managing training programs"
+    });
+
+    // Adding XML comments for controller summaries
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfile).Assembly);
 
 builder.Services.AddDbContext<TrainingManagerDbContext>(options =>
